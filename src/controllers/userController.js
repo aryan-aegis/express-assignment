27cbac9320ae492eacc728cf187f9213e943dad5
Tejush <tejush@aegiscovenant.com>
2023-01-18 16:06:08 +0530

    Api

---
+++ src/controllers/userController.js
@@ -0,0 +1,19 @@
+import { PrismaClient }from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+let createUser = async function(req,res){
+
+    let data = req.body
+    console.log(data)
+
+    const newUser = await prisma.user.create({
+        data
+    }) 
+
+    // console.log(newUser)
+    res.status(201).send({status:true, newUser})
+
+}
+
+export {createUser}
