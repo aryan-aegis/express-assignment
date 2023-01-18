587fb1245deae5bf9b527e717e5f318ff7f976f1
AnkitAegis <ankit@aegiscovenant.com>
2023-01-18 17:29:30 +0530

    todo controller & routes added

---
+++ src/routes/todo.routes.js
@@ -0,0 +1,14 @@
+import {Router} from "express";
+import {createTodo,deleteTodo,patchTodo,getTodo} from "../controllers/todoController"
+
+let todoRoute = Router();
+
+todoRoute.get("/",getTodo)
+
+todoRoute.post("/",createTodo)
+
+todoRoute.patch("/",patchTodo)
+
+todoRoute.delete("/",deleteTodo)
+
+export default todoRoute;
