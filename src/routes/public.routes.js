cd33b4e4063000ca787d44843efcfc8604972b55
AnkitAegis <ankit@aegiscovenant.com>
2023-01-19 11:08:40 +0530

    wait for merging this

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { updateUser, createUser } from '../controllers/userController'
+import { updateUser, createUser } from '../controllers/userController.js'
 const router = Router()
 
 router.post('/createUser', createUser)
