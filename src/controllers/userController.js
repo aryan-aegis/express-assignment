a0b6339e27f91447c197fae1e1626cad14deaa8b
aryan-aegis <122883593+aryan-aegis@users.noreply.github.com>
2023-01-23 00:22:15 +0530

    Jsdoc (#10)
    * JSDOCS integration added for all controllers and validation functions
    
    Co-authored-by: aryan-aegis <aryan@aegisconvenant.com>

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,23 +2,39 @@ import { PrismaClient } from '@prisma/client'
 import validateEmail from '../validation/EmailVailidation.js'
 import validateNumber from '../validation/PhoneValidation.js'
 
+/**
+ * @typedef {Object} userBody
+ * @property {string} username - The name of the user
+ * @property {string} email - Email of User
+ * @property {string} phone - Phone Number of User
+ */
+
 const prisma = new PrismaClient()
 
+/**
+ * @function createUser - Creates a new User
+ * @param {Express.Request} req - Express Request Object containing the User data in the body {@link userBody}
+ * @param {Express.Response} res
+ */
 export const createUser = async function (req, res) {
   let data = req.body
-  console.log(data)
   if (!validateEmail(data.email)) {
     res.status(400).send({ status: false, message: 'Enter valid email' })
   } else if (!validateNumber(data.phone)) {
     res.status(400).send({ status: false, message: 'Enter valid phone number' })
   } else {
     const newUser = await prisma.user.create({
-      data,
+      data
     })
     res.status(201).send({ status: true, newUser })
   }
 }
 
+/**
+ * @function updateUser - Updates the information of an User.
+ * @param {Express.Request} req - Express Request Object containing the User data in the body {@link userBody}
+ * @param {Express.Response} res
+ */
 export const updateUser = async (req, res) => {
   const { email, username, phone } = req.body
   if (!validateEmail(email)) {
@@ -31,16 +47,21 @@ export const updateUser = async (req, res) => {
       data: {
         email,
         username,
-        phone,
-      },
+        phone
+      }
     })
     res.status(201).send({ status: true, user })
   }
 }
 
+/**
+ * @function getUser - Gets the information of an User based on the ID
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ */
 export const getUser = async function (req, res) {
-  let data = req.params;
-  console.log(data)
+  let data = req.params
+
   try {
     const user = await prisma.user.findUnique({
       where: {
@@ -48,7 +69,9 @@ export const getUser = async function (req, res) {
       }
     })
 
-    res.status(200).send({ user, message: 'user found with username' +user.username})
+    res
+      .status(200)
+      .send({ user, message: 'user found with username' + user.username })
   } catch (e) {
     res.status(401).send({ message: 'Not fullfilled', error: e.message })
   }
--- src/validation/PhoneValidation.js
+++ src/validation/PhoneValidation.js
@@ -1,3 +1,9 @@
+/**
+ * @function validateNumber - To validate a phone Number
+ * @param {string} input_str - The Number that needs to be validated
+ * @returns {boolean}  returns true if the given string is a valid  phone numnber
+ */
+
 function validateNumber(input_str) {
   var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/
 
