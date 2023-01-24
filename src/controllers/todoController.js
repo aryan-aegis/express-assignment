2528767da81c6c751b51a28b7a5860b4ce534c56
Heemangi-aegis <122961018+Heemangi-aegis@users.noreply.github.com>
2023-01-24 13:53:29 +0530

    test cases added (#14)

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -45,6 +45,7 @@ const getSingleTodo = async (req, res) => {
 
     res.status(200).send({ uniqueToDo, message: 'get todo done with content ' })
   } catch (e) {
+    console.log(e)
     res.status(500).send({ message: 'Not fullfilled', error: e.message })
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import morgan from 'morgan'
 import { PORT } from './env.js'
 import router from './routes/public.routes.js'
 import todoRouter from './routes/todo.routes.js'
 
 const app = express()
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.use('/user', router)
 app.use('/todo', todoRouter)
