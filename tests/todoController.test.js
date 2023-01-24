73592e9442d2fc8c85062d03398402af99a7cbde
Heemangi <heemangi@aegiscovenant.com>
2023-01-24 16:50:19 +0530

    Added all valid tests for ToDo

--- tests/todoController.test.js
+++ tests/todoController.test.js
@@ -1,7 +1,39 @@
 const request = require("supertest")
 const baseURL = "http://localhost:5000"
 
-describe("GET /todos", () => {
+describe("CREATE /todos", () => {
+    const newTodo = {
+    userId: 1,
+    content: "Drink water",
+    done: false
+    }
+    let createdToDoMain = null;
+    it("Message should match", async () => {
+        createdToDoMain = await JSON.parse((await request(baseURL).post("/todo").send(newTodo)).text);
+        expect(createdToDoMain.message).toBe("todo created");
+    });
+    afterAll(async () => {
+        await request(baseURL).delete(`/todo/${createdToDoMain.newTodo.id}`);
+    })
+});
+
+describe("DELETE /todos", () => {
+    const newTodo = {
+    userId: 1,
+    content: "Drink water",
+    done: false
+    }    
+    let createdToDo = null;
+    beforeAll(async () => {
+        createdToDo =  await JSON.parse((await request(baseURL).post("/todo").send(newTodo)).text).newTodo;
+    })
+    it("Message should match", async () => {
+        let deletedToDo = await JSON.parse((await request(baseURL).delete(`/todo/${createdToDo.id}`)).text);
+        expect(deletedToDo.message).toBe("todo deleted");
+    });
+});
+
+describe("GETSINGLETODO /todos", () => {
     const newTodo = {
     userId: 1,
     content: "Drink water",
@@ -21,18 +53,58 @@ describe("GET /todos", () => {
     })
 });
 
-describe("CREATE /todos", () => {
+describe("PATCH /todos", () => {
     const newTodo = {
     userId: 1,
     content: "Drink water",
     done: false
     }
-    let createdToDoMain = null;
-    it("Message should match", async () => {
-        createdToDoMain = await JSON.parse((await request(baseURL).post("/todo").send(newTodo)).text);
-        expect(createdToDoMain.message).toBe("todo created");
+    const updateToDo = {
+        userId: 1,
+        content: "Drink water",
+        done: true
+    }
+    let createdToDo = null;
+    beforeAll(async () => {
+        createdToDo =  await JSON.parse((await request(baseURL).post("/todo").send(newTodo)).text).newTodo;
+    })
+    it("should return 200", async () => {
+        const response =  await request(baseURL).patch(`/todo/${createdToDo.id}`).send(updateToDo);
+        expect(response.status).toBe(200);
+        expect(response.body.error).toBe(undefined);
     });
     afterAll(async () => {
-        await request(baseURL).delete(`/todo/${createdToDoMain.newTodo.id}`);
+        await request(baseURL).delete(`/todo/${createdToDo.id}`);
     })
 });
+
+describe("GETALLTODO /todos", () => {
+    jest.setTimeout(30000)
+    const newTodo = {
+    userId: 1,
+    content: "Drink water",
+    done: false
+    }
+    const newTodoagain = {
+        userId: 1,
+        content: "Drink water again",
+        done: false
+    }
+    let createdToDo = null;
+    let createdToDoagain = null;
+    beforeAll(async () => {
+        createdToDo =  await JSON.parse((await request(baseURL).post("/todo").send(newTodo)).text).newTodo;
+        createdToDoagain =  await JSON.parse((await request(baseURL).post("/todo").send(newTodo)).text).newTodo;
+    })
+    it("should return 200", async () => {
+        const resultGetAll = (await request(baseURL).get(`/todo/all/1`))
+        const response = await JSON.parse(resultGetAll.text).userDocs;
+        const length = await response.length;
+        expect(length).toBeGreaterThan(1);
+        expect(resultGetAll.status).toBe(200);
+    });
+    afterAll(async () => {
+        await request(baseURL).delete(`/todo/${createdToDo.id}`);
+        await request(baseURL).delete(`/todo/${createdToDoagain.id}`);
+    })
+})
