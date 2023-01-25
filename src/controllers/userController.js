839fb46692a212cc9511b8c7dd0599ca929dd564
AnkitAegis <ankit@aegiscovenant.com>
2023-01-25 15:04:25 +0530

    local conflict fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import {validateEmail,validateNumber} from '../validation/userValidation.js'
+import { validateEmail, validateNumber } from '../validation/userValidation.js'
 
 /**
  * @typedef {Object} userBody
@@ -18,24 +18,38 @@ const prisma = new PrismaClient()
 export const createUser = async function (req, res) {
   let data = req.body
 
-  let keyArray = ["username","phone","email"]
-  if(!Object.keys(req.body).every((elem) => keyArray.includes(elem)) || Object.keys(req.body).length !=3)
-  return res.status(400).send({message:"All keys should be given and in this format ['username','phone','email'] "})
+  let keyArray = ['username', 'phone', 'email']
+  if (
+    !Object.keys(req.body).every((elem) => keyArray.includes(elem)) ||
+    Object.keys(req.body).length != 3
+  )
+    return res
+      .status(400)
+      .send({
+        message:
+          "All keys should be given and in this format ['username','phone','email'] "
+      })
 
-  if (!validateEmail(data.email)) 
+  if (!validateEmail(data.email))
     return res.status(400).send({ status: false, message: 'Enter valid email' })
-  if (!validateNumber(data.phone)) 
-    return res.status(400).send({ status: false, message: 'Enter valid phone number' })
+  if (!validateNumber(data.phone))
+    return res
+      .status(400)
+      .send({ status: false, message: 'Enter valid phone number' })
 
-    let newUser=null
+  let newUser = null
 
-    try{ newUser = await prisma.user.create({
+  try {
+    newUser = await prisma.user.create({
       data
-    })}
-    catch(err)
-    {return res.status(400).send({message:"phonenumber and email should be unique"})}
+    })
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ message: 'phonenumber and email should be unique' })
+  }
 
-    return res.status(201).send({ status: true, newUser })
+  return res.status(201).send({ status: true, newUser })
 }
 
 /**
@@ -47,27 +61,35 @@ export const updateUser = async (req, res) => {
   const { email, username, phone } = req.body
   const id = req.params.id
 
-  let keyArray = ["username","phone","email"]
-  if(!Object.keys(req.body).every((elem) => keyArray.includes(elem)))
-  return res.status(400).send({message:"Keys should be in this format ['username','phone','email'] "})
+  let keyArray = ['username', 'phone', 'email']
+  if (!Object.keys(req.body).every((elem) => keyArray.includes(elem)))
+    return res
+      .status(400)
+      .send({
+        message: "Keys should be in this format ['username','phone','email'] "
+      })
 
-
-  if (!validateEmail(email)) 
+  if (!validateEmail(email))
     return res.status(400).send({ status: false, message: 'Enter valid email' })
-  if (!validateNumber(phone)) 
-    return res.status(400).send({ status: false, message: 'Enter valid phone number' })
- 
-    let user=null
-   try{  user = await prisma.user.update({
-      where: { id:+id },
+  if (!validateNumber(phone))
+    return res
+      .status(400)
+      .send({ status: false, message: 'Enter valid phone number' })
+
+  let user = null
+  try {
+    user = await prisma.user.update({
+      where: { id: +id },
       data: {
         email,
         username,
         phone
       }
-    })}catch(err)
-    {return res.status(404).send({message:"User not found with given id"})}
-    return res.status(200).send({ status: true, user })
+    })
+  } catch (err) {
+    return res.status(404).send({ message: 'User not found with given id' })
+  }
+  return res.status(200).send({ status: true, user })
 }
 
 /**
@@ -77,28 +99,27 @@ export const updateUser = async (req, res) => {
  */
 export const getUser = async function (req, res) {
   let data = req.params
-    const user = await prisma.user.findUnique({
-      where: {
-        id: +data.id
-    }})
+  const user = await prisma.user.findUnique({
+    where: {
+      id: +data.id
+    }
+  })
 
-    if(!user)
-    return res.status(404).send({message:"user not found"})
+  if (!user) return res.status(404).send({ message: 'user not found' })
 
-    return res
-      .status(200)
-      .send({ user, message: 'user found with username ' + user.username })
-
+  return res
+    .status(200)
+    .send({ user, message: 'user found with username ' + user.username })
 }
 
 export const deleteUser = async (req, res) => {
   const { id } = req.params
 
-    const delDoc = await prisma.User.delete({
-      where: {
-        id: +id
-      }
-    })
+  const delDoc = await prisma.User.delete({
+    where: {
+      id: +id
+    }
+  })
 
-    return res.status(200).send({ delDoc, message: 'todo deleted' })
+  return res.status(200).send({ delDoc, message: 'todo deleted' })
 }
