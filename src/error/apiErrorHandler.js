5dd141e07b1988e37f2ffe4eb0e81ab1591cc81d
Tejush <tejush@aegiscovenant.com>
2023-01-26 19:46:27 +0530

    elseif

--- src/error/apiErrorHandler.js
+++ src/error/apiErrorHandler.js
@@ -5,13 +5,9 @@ import { Prisma } from '@prisma/client'
 export const apiErrorHandler = (err, req, res, next) => {
   if (err instanceof ApiError) {
     return res.status(err.code).json(err.message)
-  }
-
-  else if (err instanceof Prisma.PrismaClientValidationError) {
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
     return res.status(400).json('Check fields provided')
-  }
-
-  else if (err.code == 'P2002')
+  } else if (err.code == 'P2002')
     return res.status(400).json('Email or phone should be unique')
 
   res.status(500).json(err.message)
